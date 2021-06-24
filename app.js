f6b5144205e93c553865cb5e6abe3bbfd20693ca
Martin Naylor <martinnaylor88@gmail.com>
2021-06-24 16:23:15 +0100

    javascript added and buttons appear on screen

---
+++ app.js
@@ -0,0 +1,67 @@
+
+console.log("Hello");
+
+//display
+
+
+// var for the display at the top where button values will be printed.
+
+const display = document.querySelector(".display");
+
+
+
+
+
+// buttons 
+
+//var for buttons using Q-S-A to select all buttons
+const buttons = document.querySelectorAll('button');
+
+
+//function to make the clicked buttons appear in the display.
+
+let answer = (e) => {
+  const clickedButton = e.target.value;
+
+if (clickedButton === 'C') {  // clears on click and returns empty string.
+  display.value = '';
+}
+else {
+      
+  display.value += clickedButton;  //adds the clicked button to the current display value 
+} 
+
+
+}
+//  //if pressed button === value "=" display the answer.
+
+
+//function to make the clicked buttons appear in the display.
+
+// let answer = (e) => {
+//   const buttonClicked = e.target.value;
+   
+  
+//   if (display.value += buttonClicked.value) {
+    
+
+
+//    } else if (buttonClicked === '=') {
+//      display.value = eval(display.value);
+//    } else if (display.value !== '') {
+//      display.value = eval(display.value);
+   
+
+
+
+// // // if button "C" is pressed return to normal
+//  } else if (clickedButtonValue === 'C') {
+      
+//        display.value = '';
+
+//  
+//event listener for the buttons
+buttons.forEach((button) => {
+  button.addEventListener('click', answer);
+})
+// i think i need to add the var answer to this so they go to the display. I had to move this to the bottom as needs to go after the function above to have answer called
